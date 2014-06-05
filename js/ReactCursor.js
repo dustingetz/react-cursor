e6de97d79b2306094839f909b8add256feb5fa16
Daniel Miladinov <daniel.miladinov@gmail.com>
2014-06-05 18:32:51 -0400

    Make ReactCursors that share paths also share onChange handler references

--- js/ReactCursor.js
+++ js/ReactCursor.js
@@ -1,4 +1,4 @@
-define(['react', 'CursorFunctions'], function (ReactWithAddons, cf) {
+define(['react', 'underscore', 'CursorFunctions'], function (ReactWithAddons, _, cf) {
     'use strict';
 
 
@@ -11,27 +11,45 @@ define(['react', 'CursorFunctions'], function (ReactWithAddons, cf) {
             return cf.getRefAtPath(pendingGetter(), path); // the current value right now, use in event handlers
         };
 
-        this.onChange = function (nextValue) {
-            var nextState;
-
-            if (path.length > 0) {
-                nextState = ReactWithAddons.addons.update(
-                    pendingGetter(),
-                    path.concat('$set').reduceRight(cf.unDeref, nextValue)
-                );
-            }
-            else if (path.length === 0) {
-                nextState = nextValue;
-            }
-            commit(nextState);
-            return new Cursor(state, pendingGetter, path, commit);
-        };
+        // Cursors sharing a path also share an onChange handler - so that we can do meaningful reference equality
+        // comparisons for onChange handlers passes as react props in shouldComponentUpdate
+        this.onChange = partialMemoized(onChange, state, pendingGetter, path, commit);
 
         this.refine = function (/* one or more paths through the tree */) {
             var nextPath = [].concat(path, cf.flatten(arguments));
             return new Cursor(state, pendingGetter, nextPath, commit);
         };
     }
+
+    function onChange (state, pendingGetter, path, commit, nextValue) {
+        var nextState;
+
+        if (path.length > 0) {
+            nextState = ReactWithAddons.addons.update(
+                pendingGetter(),
+                path.concat('$set').reduceRight(cf.unDeref, nextValue)
+            );
+        }
+        else if (path.length === 0) {
+            nextState = nextValue;
+        }
+        commit(nextState);
+        return new Cursor(state, pendingGetter, path, commit);
+    }
+
+    /**
+     * Given all of the arguments of a memoized onChange function, the only discriminator is the path.
+     * An onChange closing over different state values but having the same path is effectively the same
+     * onChange for the purposes of effecting change in a reference-equality-sensitive manner.
+     */
+    function memoHasher (onChange, state, pendingGetter, path, commit) {
+        return cf.hashRecord(path);
+    }
+
+    /**
+     * Return existing partial functions for arguments we've seen before - the memoHasher will provide us the hash keys
+     */
+    var partialMemoized = _.memoize(_.partial, memoHasher);
 
     Cursor.build = function (state, pendingGetter, commit) {
         return new Cursor(state, pendingGetter, [], commit);
