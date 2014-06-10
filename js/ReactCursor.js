4bc65447bb4dacc0d31abfc283262222659b6ddd
Daniel Miladinov <dmiladinov@wingspan.com>
2014-06-10 14:48:02 -0400

    Every invocation of ReactCursor.build creates a new onChange cache for that Cursor

--- js/ReactCursor.js
+++ js/ReactCursor.js
@@ -2,7 +2,7 @@ define(['react', 'underscore', 'CursorFunctions'], function (ReactWithAddons, _,
     'use strict';
 
 
-    function Cursor(state, pendingGetter, path, commit) {
+    function Cursor(state, pendingGetter, path, commit, partialMemoized) {
         // Please treat values as read-only
         this.value = cf.getRefAtPath(state, path); // value to put in the DOM, use from render()
 
@@ -13,15 +13,15 @@ define(['react', 'underscore', 'CursorFunctions'], function (ReactWithAddons, _,
 
         // Cursors sharing a path also share an onChange handler - so that we can do meaningful reference equality
         // comparisons for onChange handlers passes as react props in shouldComponentUpdate
-        this.onChange = partialMemoized(onChange, state, pendingGetter, path, commit);
+        this.onChange = partialMemoized(onChange, state, pendingGetter, path, commit, partialMemoized);
 
         this.refine = function (/* one or more paths through the tree */) {
             var nextPath = [].concat(path, cf.flatten(arguments));
-            return new Cursor(state, pendingGetter, nextPath, commit);
+            return new Cursor(state, pendingGetter, nextPath, commit, partialMemoized);
         };
     }
 
-    function onChange (state, pendingGetter, path, commit, nextValue) {
+    function onChange(state, pendingGetter, path, commit, partialMemoized, nextValue) {
         var nextState;
 
         if (path.length > 0) {
@@ -34,25 +34,33 @@ define(['react', 'underscore', 'CursorFunctions'], function (ReactWithAddons, _,
             nextState = nextValue;
         }
         commit(nextState);
-        return new Cursor(state, pendingGetter, path, commit);
+        return new Cursor(state, pendingGetter, path, commit, partialMemoized);
     }
-
-    /**
-     * Given all of the arguments of a memoized onChange function, the only discriminator is the path.
-     * An onChange closing over different state values but having the same path is effectively the same
-     * onChange for the purposes of effecting change in a reference-equality-sensitive manner.
-     */
-    function memoHasher (onChange, state, pendingGetter, path, commit) {
-        return cf.hashRecord(path);
-    }
-
-    /**
-     * Return existing partial functions for arguments we've seen before - the memoHasher will provide us the hash keys
-     */
-    var partialMemoized = _.memoize(_.partial, memoHasher);
 
     Cursor.build = function (state, pendingGetter, commit) {
-        return new Cursor(state, pendingGetter, [], commit);
+        // Maintain a per-cursor cache of partially applied onChange functions - paths are not global, they are specific
+        // to an initial call to Cursor.build
+        var cache = {};
+
+        function memoize(func, resolver) {
+            return function () {
+                var key = resolver ? resolver.apply(this, arguments) : arguments[0];
+                return hasOwnProperty.call(cache, key)
+                    ? cache[key]
+                    : (cache[key] = func.apply(this, arguments));
+            };
+        }
+
+        /**
+         * Given all of the arguments of a memoized onChange function, the only discriminator is the path.
+         * An onChange closing over different state values but having the same path is effectively the same
+         * onChange for the purposes of effecting change in a reference-equality-sensitive manner.
+         */
+        function memoHasher(onChange, state, pendingGetter, path, commit) {
+            return cf.hashRecord(path);
+        }
+
+        return new Cursor(state, pendingGetter, [], commit, memoize(_.partial, memoHasher));
     };
 
     return Cursor;
