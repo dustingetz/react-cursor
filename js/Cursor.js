5a0aca866bc444fca5ad6b1708f843c8963d1361
Dustin Getz <dustin.getz@gmail.com>
2014-07-09 20:42:28 -0400

    It works

--- js/Cursor.js
+++ js/Cursor.js
@@ -1,7 +1,7 @@
 define(['react', 'util'], function (React, util) {
   'use strict';
 
-  function Cursor(state, pendingGetter, path, commit, partialMemoized) {
+  function Cursor(cmp, state, pendingGetter, path, commit, partialMemoized) {
     // Please treat values as read-only
     this.value = util.getRefAtPath(state, path); // value to put in the DOM, use from render()
 
@@ -12,7 +12,7 @@ define(['react', 'util'], function (React, util) {
 
     // Cursors sharing a path also share an onChange handler - so that we can do meaningful reference equality
     // comparisons for onChange handlers passes as react props in shouldComponentUpdate
-    this.onChange = partialMemoized(onChange, state, pendingGetter, path, commit, partialMemoized);
+    this.onChange = partialMemoized(onChange, cmp, state, pendingGetter, path, commit, partialMemoized);
 
     this.refine = function (/* one or more paths through the tree */) {
       var nextPath = [].concat(path, util.flatten(arguments));
@@ -20,7 +20,7 @@ define(['react', 'util'], function (React, util) {
     };
   }
 
-  function onChange(state, pendingGetter, path, commit, partialMemoized, nextValue) {
+  function onChange(cmp, state, pendingGetter, path, commit, partialMemoized, nextValue) {
     var nextState;
 
     if (path.length > 0) {
@@ -33,12 +33,12 @@ define(['react', 'util'], function (React, util) {
       nextState = nextValue;
     }
     commit(nextState);
-    return new Cursor(state, pendingGetter, path, commit, partialMemoized);
+    return new Cursor(cmp, state, pendingGetter, path, commit, partialMemoized);
   }
 
 
   // build is memoized at global scope
-  var cursorBuildMemoize = util.memoizeFactory(function cursorBuildHasher (cmp) {
+  var cursorBuildMemoizer = util.memoizeFactory(function cursorBuildHasher (cmp) {
     // build should memoize globally on: cmp ref, and cmp.state value
     return util.refToHash(cmp) + util.hashRecord(cmp.state);
     // I think we want to clamp this to cachesize === 2, because we only
@@ -46,21 +46,24 @@ define(['react', 'util'], function (React, util) {
   });
 
 
-  Cursor.build = cursorBuildMemoize(function (cmp) {
+  // Maintain a per-cursor cache of partially applied onChange functions - paths are not global,
+  // they are specific to an initial call to Cursor.build
+  //
+  // If we want two cursor.build calls to have the same change handlers, we have to memozie on the cmp too,
+  // then it can be global
+  //
+  var onChangeMemoizer = util.memoizeFactory(function memoHasher(onChange, cmp, state, pendingGetter, path, commit) {
+    /**
+     * Given all of the arguments of a memoized onChange function, the only discriminator is the path.
+     * An onChange closing over different state values but having the same path is effectively the same
+     * onChange for the purposes of effecting change in a reference-equality-sensitive manner.
+     */
+    return util.refToHash(cmp) + util.hashRecord(path);
+  });
+
+  Cursor.build = cursorBuildMemoizer(function (cmp) {
     function pendingGetter () { return cmp._pendingState || cmp.state; }
-
-    // Maintain a per-cursor cache of partially applied onChange functions - paths are not global,
-    // they are specific to an initial call to Cursor.build
-    var memoize = util.memoizeFactory(function memoHasher(onChange, state, pendingGetter, path, commit) {
-      /**
-       * Given all of the arguments of a memoized onChange function, the only discriminator is the path.
-       * An onChange closing over different state values but having the same path is effectively the same
-       * onChange for the purposes of effecting change in a reference-equality-sensitive manner.
-       */
-      return util.hashRecord(path);
-    });
-
-    return new Cursor(cmp.state, pendingGetter, [], cmp.setState.bind(cmp), memoize(_.partial));
+    return new Cursor(cmp, cmp.state, pendingGetter, [], cmp.setState.bind(cmp), onChangeMemoizer(_.partial));
   });
 
 
