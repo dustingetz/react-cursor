61aace76f054e3951455cd3b2c8f563c9cb87643
Dustin Getz <dustin.getz@gmail.com>
2014-07-10 17:23:36 -0400

    Refining during lifecycle methods requires care when handling the value

--- js/Cursor.js
+++ js/Cursor.js
@@ -2,9 +2,9 @@ define(['react', 'util'], function (React, util) {
   'use strict';
 
 
-  function Cursor(cmp, path) {
+  function Cursor(cmp, path, value) {
     // value to put in the DOM, use from render() and the component lifecycle methods
-    this.value = util.getRefAtPath(cmp.state, path);
+    this.value = value;
 
     this.pendingValue = function () {
       // the current value right now, use in event handlers
@@ -14,8 +14,12 @@ define(['react', 'util'], function (React, util) {
     this.onChange = _.partial(onChange, cmp, path);
 
     this.refine = function (/* one or more paths through the tree */) {
+      // When refining inside a lifecycle method, same cmp and same path isn't enough.
+      // this.props and nextProps have different subtree values, and refining memoizer must account for that
+
       var nextPath = [].concat(path, util.flatten(arguments));
-      return build(cmp, nextPath); // memoized
+      var nextValue = util.getRefAtPath(this.value, _.toArray(arguments));
+      return build(cmp, nextPath, nextValue); // memoized
     };
   }
 
@@ -38,17 +42,18 @@ define(['react', 'util'], function (React, util) {
   // If we build two cursors for the same path on the same React component,
   // and those React components have equal state, reuse the same cursor instance,
   // so we can use === to compare them.
-  var cursorBuildMemoizer = util.memoizeFactory(function (cmp, path) {
-    path = path === undefined ? [] : path; // account for the default value here
-    var subtreeValue = util.getRefAtPath(cmp.state, path);
-    return util.refToHash(cmp) + util.hashRecord(subtreeValue) + util.hashRecord(path);
+  var cursorBuildMemoizer = util.memoizeFactory(function (cmp, path, value) {
+    path = path === undefined ? [] : path;
+    value = value || util.getRefAtPath(cmp.state, path);
+    return util.refToHash(cmp) + util.hashRecord(value) + util.hashRecord(path);
     // I think we want to clamp this to cachesize === 2, because we only
     // care about this.state and nextState.
   });
 
-  var build = cursorBuildMemoizer(function (cmp, path) {
+  var build = cursorBuildMemoizer(function (cmp, path, value) {
     path = path === undefined ? [] : path;
-    return new Cursor(cmp, path);
+    value = value || util.getRefAtPath(cmp.state, path);
+    return new Cursor(cmp, path, value);
   });
 
 
