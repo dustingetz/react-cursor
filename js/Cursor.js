cdc3ce022b78eeb02fbcec3d5a42ac935af5c773
Dustin Getz <dustin.getz@gmail.com>
2014-07-09 21:32:02 -0400

    Move memoize fns to the top

--- js/Cursor.js
+++ js/Cursor.js
@@ -1,6 +1,25 @@
 define(['react', 'util'], function (React, util) {
   'use strict';
 
+
+  // If we build two cursors on the same React component, and those React components have equal state,
+  // reuse the same cursor instance, so we can use === to compare them.
+  var cursorBuildMemoizer = util.memoizeFactory(function (cmp) {
+    return util.refToHash(cmp) + util.hashRecord(cmp.state);
+    // I think we want to clamp this to cachesize === 2, because we only
+    // care about this.state and nextState.
+  });
+
+  // We want two cursors of the same path to share the same reference.
+  // If we call cursor.build twice, the onChange handlers should still share the same reference.
+  // So we have a global cache of partially applied onChange functions, so we can reuse onChange functions
+  // if both the paths are the same, and they are attached to the same React component.
+  // Note we don't care about the state of the React component for onChange handlers.
+  var onChangeMemoizer = util.memoizeFactory(function (onChange, cmp, path) {
+    return util.refToHash(cmp) + util.hashRecord(path);
+  });
+
+
   function Cursor(cmp, path, partialMemoized) {
     // Please treat values as read-only
     this.value = util.getRefAtPath(cmp.state, path); // value to put in the DOM, use from render()
@@ -35,23 +54,6 @@ define(['react', 'util'], function (React, util) {
     cmp.setState(nextState);
     return new Cursor(cmp, path, partialMemoized);
   }
-
-  // If we build two cursors on the same React component, and those React components have equal state,
-  // reuse the same cursor instance, so we can use === to compare them.
-  var cursorBuildMemoizer = util.memoizeFactory(function (cmp) {
-    return util.refToHash(cmp) + util.hashRecord(cmp.state);
-    // I think we want to clamp this to cachesize === 2, because we only
-    // care about this.state and nextState.
-  });
-
-  // We want two cursors of the same path to share the same reference.
-  // If we call cursor.build twice, the onChange handlers should still share the same reference.
-  // So we have a global cache of partially applied onChange functions, so we can reuse onChange functions
-  // if both the paths are the same, and they are attached to the same React component.
-  // Note we don't care about the state of the React component for onChange handlers.
-  var onChangeMemoizer = util.memoizeFactory(function (onChange, cmp, path) {
-    return util.refToHash(cmp) + util.hashRecord(path);
-  });
 
   Cursor.build = cursorBuildMemoizer(function (cmp) {
     return new Cursor(cmp, [], onChangeMemoizer(_.partial));
