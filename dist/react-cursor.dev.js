bfe214a355b9d2dda3f21fd3599b76ecc67531f3
Nik Voss <niklas.voss@gmail.com>
2014-09-24 20:30:08 +0200

    Rebuild project and example.

--- dist/react-cursor.dev.js
+++ dist/react-cursor.dev.js
@@ -20823,7 +20823,7 @@ Cursor.debug = false;
 module.exports = Cursor;
 
 
-},{"./util":173,"react/addons":10}],170:[function(require,module,exports){
+},{"./util":172,"react/addons":10}],170:[function(require,module,exports){
 var utils = require('./util');
 
 'use strict';
@@ -20847,167 +20847,7 @@ function ImmutableOptimizations (refFields, ignoredFields/*optional*/) {
 
 module.exports = ImmutableOptimizations;
 
-},{"./util":173}],171:[function(require,module,exports){
-'use strict';
-
-// From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind
-if (!Function.prototype.bind) {
-  Function.prototype.bind = function (context) {
-    if (typeof this !== "function") {
-      // closest thing possible to the ECMAScript 5
-      // internal IsCallable function
-      throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
-    }
-
-    var args = Array.prototype.slice.call(arguments, 1),
-        toBind = this,
-        NOP = function () {},
-        bound = function () {
-          return toBind.apply(this instanceof NOP && context
-                 ? this
-                 : context,
-                 args.concat(Array.prototype.slice.call(arguments)));
-        };
-
-    NOP.prototype = this.prototype;
-    bound.prototype = new NOP();
-
-    return bound;
-  };
-}
-
-// From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
-if (!Object.keys) {
-  Object.keys = (function() {
-    'use strict';
-    var hasOwnProperty = Object.prototype.hasOwnProperty,
-        hasDontEnumBug = !({ toString: null }).propertyIsEnumerable('toString'),
-        dontEnums = [
-          'toString',
-          'toLocaleString',
-          'valueOf',
-          'hasOwnProperty',
-          'isPrototypeOf',
-          'propertyIsEnumerable',
-          'constructor'
-        ],
-        dontEnumsLength = dontEnums.length;
-
-    return function(obj) {
-      if (typeof obj !== 'object' && (typeof obj !== 'function' || obj === null)) {
-        throw new TypeError('Object.keys called on non-object');
-      }
-
-      var result = [], prop, i;
-
-      for (prop in obj) {
-        if (hasOwnProperty.call(obj, prop)) {
-          result.push(prop);
-        }
-      }
-
-      if (hasDontEnumBug) {
-        for (i = 0; i < dontEnumsLength; i++) {
-          if (hasOwnProperty.call(obj, dontEnums[i])) {
-            result.push(dontEnums[i]);
-          }
-        }
-      }
-      return result;
-    };
-  }());
-}
-
-// From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
-if (!Array.prototype.find) {
-  Array.prototype.find = function(predicate) {
-    if (this == null) {
-      throw new TypeError('Array.prototype.find called on null or undefined');
-    }
-    if (typeof predicate !== 'function') {
-      throw new TypeError('predicate must be a function');
-    }
-    var list = Object(this);
-    var length = list.length >>> 0;
-    var thisArg = arguments[1];
-    var value;
-
-    for (var i = 0; i < length; i++) {
-      value = list[i];
-      if (predicate.call(thisArg, value, i, list)) {
-        return value;
-      }
-    }
-    return undefined;
-  };
-}
-
-// From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every
-if (!Array.prototype.every) {
-  Array.prototype.every = function(callbackfn, thisArg) {
-    'use strict';
-    var T, k;
-
-    if (this == null) {
-      throw new TypeError('this is null or not defined');
-    }
-
-    // 1. Let O be the result of calling ToObject passing the this
-    //    value as the argument.
-    var O = Object(this);
-
-    // 2. Let lenValue be the result of calling the Get internal method
-    //    of O with the argument "length".
-    // 3. Let len be ToUint32(lenValue).
-    var len = O.length >>> 0;
-
-    // 4. If IsCallable(callbackfn) is false, throw a TypeError exception.
-    if (typeof callbackfn !== 'function') {
-      throw new TypeError();
-    }
-
-    // 5. If thisArg was supplied, let T be thisArg; else let T be undefined.
-    if (arguments.length > 1) {
-      T = thisArg;
-    }
-
-    // 6. Let k be 0.
-    k = 0;
-
-    // 7. Repeat, while k < len
-    while (k < len) {
-
-      var kValue;
-
-      // a. Let Pk be ToString(k).
-      //   This is implicit for LHS operands of the in operator
-      // b. Let kPresent be the result of calling the HasProperty internal
-      //    method of O with argument Pk.
-      //   This step can be combined with c
-      // c. If kPresent is true, then
-      if (k in O) {
-
-        // i. Let kValue be the result of calling the Get internal method
-        //    of O with argument Pk.
-        kValue = O[k];
-
-        // ii. Let testResult be the result of calling the Call internal method
-        //     of callbackfn with T as the this value and argument list
-        //     containing kValue, k, and O.
-        var testResult = callbackfn.call(T, kValue, k, O);
-
-        // iii. If ToBoolean(testResult) is false, return false.
-        if (!testResult) {
-          return false;
-        }
-      }
-      k++;
-    }
-    return true;
-  };
-}
-
-},{}],172:[function(require,module,exports){
+},{"./util":172}],171:[function(require,module,exports){
 var Cursor = require('./Cursor');
 var ImmutableOptimizations = require('./ImmutableOptimizations');
 
@@ -21018,14 +20858,30 @@ module.exports = {
   ImmutableOptimizations: ImmutableOptimizations
 }
 
-},{"./Cursor":169,"./ImmutableOptimizations":170}],173:[function(require,module,exports){
+},{"./Cursor":169,"./ImmutableOptimizations":170}],172:[function(require,module,exports){
 'use strict';
-
-require('./polyfills'); // Load polyfills for older browsers if necessary
 
 var isEqual = require('deep-equal');
 var union = require('array-union');
 var omit = require('omit-keys');
+
+function find(array, predicate) {
+  if (typeof predicate !== 'function') {
+    throw new TypeError('predicate must be a function');
+  }
+  var list = Object(array);
+  var length = list.length >>> 0;
+  var thisArg = arguments[1];
+  var value;
+
+  for (var i = 0; i < length; i++) {
+    value = list[i];
+    if (predicate.call(thisArg, value, i, list)) {
+      return value;
+    }
+  }
+  return undefined;
+}
 
 function getRefAtPath(tree, paths) {
   return reduce(paths, deref, tree);
@@ -21111,7 +20967,7 @@ function refToHash (cmp) {
   // if not, add to cache and generate a new ID to hash on
 
   var cmpsWithUid = pairs(refsCache);
-  var cmpFound = cmpsWithUid.find(function (cmpAndId) { return cmpAndId[1] === cmp; });
+  var cmpFound = find(cmpsWithUid, function (cmpAndId) { return cmpAndId[1] === cmp; });
   if (cmpFound) {
     return cmpFound[0]; // return the uid
   }
@@ -21152,6 +21008,7 @@ module.exports = {
   isEqual: isEqual,
   union: union,
   omit: omit,
+  find: find,
 };
 
-},{"./polyfills":171,"array-union":1,"deep-equal":3,"omit-keys":7}]},{},[172]);
+},{"array-union":1,"deep-equal":3,"omit-keys":7}]},{},[171]);
--- examples/helloworld/webapp/js/build.js
+++ examples/helloworld/webapp/js/build.js
@@ -21973,7 +21973,7 @@ function entrypoint(rootEl) {
 window.entrypoint = entrypoint;
 
 
-},{"../../../../src/react-cursor":333,"react/addons":2,"underscore":161}],163:[function(require,module,exports){
+},{"../../../../src/react-cursor":332,"react/addons":2,"underscore":161}],163:[function(require,module,exports){
 'use strict';
 var arrayUniq = require('array-uniq');
 
@@ -22658,7 +22658,7 @@ Cursor.debug = false;
 module.exports = Cursor;
 
 
-},{"./util":334,"react/addons":171}],331:[function(require,module,exports){
+},{"./util":333,"react/addons":171}],331:[function(require,module,exports){
 var utils = require('./util');
 
 'use strict';
@@ -22682,167 +22682,7 @@ function ImmutableOptimizations (refFields, ignoredFields/*optional*/) {
 
 module.exports = ImmutableOptimizations;
 
-},{"./util":334}],332:[function(require,module,exports){
-'use strict';
-
-// From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind
-if (!Function.prototype.bind) {
-  Function.prototype.bind = function (context) {
-    if (typeof this !== "function") {
-      // closest thing possible to the ECMAScript 5
-      // internal IsCallable function
-      throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
-    }
-
-    var args = Array.prototype.slice.call(arguments, 1),
-        toBind = this,
-        NOP = function () {},
-        bound = function () {
-          return toBind.apply(this instanceof NOP && context
-                 ? this
-                 : context,
-                 args.concat(Array.prototype.slice.call(arguments)));
-        };
-
-    NOP.prototype = this.prototype;
-    bound.prototype = new NOP();
-
-    return bound;
-  };
-}
-
-// From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
-if (!Object.keys) {
-  Object.keys = (function() {
-    'use strict';
-    var hasOwnProperty = Object.prototype.hasOwnProperty,
-        hasDontEnumBug = !({ toString: null }).propertyIsEnumerable('toString'),
-        dontEnums = [
-          'toString',
-          'toLocaleString',
-          'valueOf',
-          'hasOwnProperty',
-          'isPrototypeOf',
-          'propertyIsEnumerable',
-          'constructor'
-        ],
-        dontEnumsLength = dontEnums.length;
-
-    return function(obj) {
-      if (typeof obj !== 'object' && (typeof obj !== 'function' || obj === null)) {
-        throw new TypeError('Object.keys called on non-object');
-      }
-
-      var result = [], prop, i;
-
-      for (prop in obj) {
-        if (hasOwnProperty.call(obj, prop)) {
-          result.push(prop);
-        }
-      }
-
-      if (hasDontEnumBug) {
-        for (i = 0; i < dontEnumsLength; i++) {
-          if (hasOwnProperty.call(obj, dontEnums[i])) {
-            result.push(dontEnums[i]);
-          }
-        }
-      }
-      return result;
-    };
-  }());
-}
-
-// From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
-if (!Array.prototype.find) {
-  Array.prototype.find = function(predicate) {
-    if (this == null) {
-      throw new TypeError('Array.prototype.find called on null or undefined');
-    }
-    if (typeof predicate !== 'function') {
-      throw new TypeError('predicate must be a function');
-    }
-    var list = Object(this);
-    var length = list.length >>> 0;
-    var thisArg = arguments[1];
-    var value;
-
-    for (var i = 0; i < length; i++) {
-      value = list[i];
-      if (predicate.call(thisArg, value, i, list)) {
-        return value;
-      }
-    }
-    return undefined;
-  };
-}
-
-// From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every
-if (!Array.prototype.every) {
-  Array.prototype.every = function(callbackfn, thisArg) {
-    'use strict';
-    var T, k;
-
-    if (this == null) {
-      throw new TypeError('this is null or not defined');
-    }
-
-    // 1. Let O be the result of calling ToObject passing the this
-    //    value as the argument.
-    var O = Object(this);
-
-    // 2. Let lenValue be the result of calling the Get internal method
-    //    of O with the argument "length".
-    // 3. Let len be ToUint32(lenValue).
-    var len = O.length >>> 0;
-
-    // 4. If IsCallable(callbackfn) is false, throw a TypeError exception.
-    if (typeof callbackfn !== 'function') {
-      throw new TypeError();
-    }
-
-    // 5. If thisArg was supplied, let T be thisArg; else let T be undefined.
-    if (arguments.length > 1) {
-      T = thisArg;
-    }
-
-    // 6. Let k be 0.
-    k = 0;
-
-    // 7. Repeat, while k < len
-    while (k < len) {
-
-      var kValue;
-
-      // a. Let Pk be ToString(k).
-      //   This is implicit for LHS operands of the in operator
-      // b. Let kPresent be the result of calling the HasProperty internal
-      //    method of O with argument Pk.
-      //   This step can be combined with c
-      // c. If kPresent is true, then
-      if (k in O) {
-
-        // i. Let kValue be the result of calling the Get internal method
-        //    of O with argument Pk.
-        kValue = O[k];
-
-        // ii. Let testResult be the result of calling the Call internal method
-        //     of callbackfn with T as the this value and argument list
-        //     containing kValue, k, and O.
-        var testResult = callbackfn.call(T, kValue, k, O);
-
-        // iii. If ToBoolean(testResult) is false, return false.
-        if (!testResult) {
-          return false;
-        }
-      }
-      k++;
-    }
-    return true;
-  };
-}
-
-},{}],333:[function(require,module,exports){
+},{"./util":333}],332:[function(require,module,exports){
 var Cursor = require('./Cursor');
 var ImmutableOptimizations = require('./ImmutableOptimizations');
 
@@ -22853,14 +22693,30 @@ module.exports = {
   ImmutableOptimizations: ImmutableOptimizations
 }
 
-},{"./Cursor":330,"./ImmutableOptimizations":331}],334:[function(require,module,exports){
+},{"./Cursor":330,"./ImmutableOptimizations":331}],333:[function(require,module,exports){
 'use strict';
-
-require('./polyfills'); // Load polyfills for older browsers if necessary
 
 var isEqual = require('deep-equal');
 var union = require('array-union');
 var omit = require('omit-keys');
+
+function find(array, predicate) {
+  if (typeof predicate !== 'function') {
+    throw new TypeError('predicate must be a function');
+  }
+  var list = Object(array);
+  var length = list.length >>> 0;
+  var thisArg = arguments[1];
+  var value;
+
+  for (var i = 0; i < length; i++) {
+    value = list[i];
+    if (predicate.call(thisArg, value, i, list)) {
+      return value;
+    }
+  }
+  return undefined;
+}
 
 function getRefAtPath(tree, paths) {
   return reduce(paths, deref, tree);
@@ -22946,7 +22802,7 @@ function refToHash (cmp) {
   // if not, add to cache and generate a new ID to hash on
 
   var cmpsWithUid = pairs(refsCache);
-  var cmpFound = cmpsWithUid.find(function (cmpAndId) { return cmpAndId[1] === cmp; });
+  var cmpFound = find(cmpsWithUid, function (cmpAndId) { return cmpAndId[1] === cmp; });
   if (cmpFound) {
     return cmpFound[0]; // return the uid
   }
@@ -22987,6 +22843,7 @@ module.exports = {
   isEqual: isEqual,
   union: union,
   omit: omit,
+  find: find,
 };
 
-},{"./polyfills":332,"array-union":163,"deep-equal":165,"omit-keys":168}]},{},[162]);
+},{"array-union":163,"deep-equal":165,"omit-keys":168}]},{},[162]);
