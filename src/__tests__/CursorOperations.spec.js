e64747fe6d4e93785f56059d42a3253dfe4f5191
Dustin Getz <dustin.getz@gmail.com>
2015-11-27 18:32:51 -0500

    run cursor operation tests against store and react-state

--- src/__tests__/CursorOperations.spec.js
+++ src/__tests__/CursorOperations.spec.js
@@ -1,82 +1,94 @@
 /* global describe, it, expect */
+import _ from 'lodash';
 import {Cursor} from '../react-cursor';
 import {Store, renderComponentWithState} from './CursorTestUtil';
 import {valEq, refEq} from '../util';
 
 
 describe('Cursor operations', () => {
-  var store, cur;
+  var cur, storeValue;
+  var initialState = {a: {b: 42}, c: [1, 2, 3]};
 
-  beforeEach(() => {
-    store = new Store({a: {b: 42}});
-    cur = Cursor.build(store.value(), store.swap);
+
+  var suite = {
+
+    'set at leaf': () => {
+      expect(storeValue()).to.deep.equal(initialState);
+      cur.refine('a', 'b').set(43);
+      expect(storeValue().a).to.deep.equal({b: 43});
+    },
+
+    'set at object': () => {
+      expect(storeValue()).to.deep.equal(initialState);
+      cur.refine('a').set({b: 43});
+      expect(storeValue().a).to.deep.equal({b: 43});
+    },
+
+    'push': () => {
+      var c = cur.refine('c');
+      c.push([4]);
+      expect(storeValue().c).to.deep.equal([1, 2, 3, 4]);
+      c.push([5, 6]);
+      expect(storeValue().c).to.deep.equal([1, 2, 3, 4, 5, 6]);
+    },
+
+    'unshift': () => {
+      var c = cur.refine('c');
+      c.unshift([0]);
+      expect(storeValue().c).to.deep.equal([0, 1, 2, 3]);
+      c.unshift(['a', 'b']);
+      expect(storeValue().c).to.deep.equal(['b', 'a', 0, 1, 2, 3]);
+    },
+
+    'splice': () => {
+      var c = cur.refine('c');
+      c.splice([[1, 1, 4]]);
+      expect(storeValue().c).to.deep.equal([1, 4, 3]);
+      c.splice([[0, 1, 6, 5], [4, 0, 2, 1]]);
+      expect(storeValue().c).to.deep.equal([6, 5, 4, 3, 2, 1]);
+    },
+
+    'merge': () => {
+      var a = cur.refine('a');
+      a.merge({ z: 1 });
+      expect(storeValue().a).to.deep.equal({ b: 42, z: 1});
+    },
+
+    'apply': () => {
+      cur.refine('a', 'b').apply(root => leaf => leaf/2);
+      expect(storeValue().a.b).to.equal(21);
+    }
+  };
+
+  describe('backed by store', () => {
+    beforeEach(() => {
+      const store = new Store(initialState);
+      cur = Cursor.build(store.value(), store.swap);
+      storeValue = () => store.value();
+    });
+
+    afterEach(() => {
+      cur = null;
+      storeValue = null;
+    });
+
+    _.map(suite, (v, k) => { it(k, v) });
   });
 
-  afterEach(() => {
-    store = null;
-    cur = null;
+
+  describe('backed by react state', () => {
+    beforeEach(() => {
+      const cmp = renderComponentWithState(initialState);
+      cur = Cursor.build(cmp);
+      storeValue = () => cmp.state;
+    });
+
+    afterEach(() => {
+      cur = null;
+      storeValue = null;
+    });
+
+    _.map(suite, (v, k) => { it(k, v) });
   });
 
-  it('set at leaf', () => {
-    expect(store.value()).to.deep.equal({a: {b: 42}});
-    cur.refine('a', 'b').set(43);
-    expect(store.value()).to.deep.equal({a: {b: 43}});
-  });
-
-  it('set at object', () => {
-    expect(store.value()).to.deep.equal({a: {b: 42}});
-    cur.refine('a').set({b: 43});
-    expect(store.value()).to.deep.equal({a: {b: 43}});
-  });
-
-  it('push', function () {
-    var cmp = renderComponentWithState({a: [1, 2, 3]});
-    var c = Cursor.build(cmp);
-    var a = c.refine('a');
-    a.push([4]);
-    expect(cmp.state.a).to.deep.equal([1, 2, 3, 4]);
-    a.push([5, 6]);
-    expect(cmp.state.a).to.deep.equal([1, 2, 3, 4, 5, 6]);
-  });
-
-  it('unshift', function () {
-    var cmp = renderComponentWithState({a: [4, 5, 6]});
-    var c = Cursor.build(cmp);
-    var a = c.refine('a');
-    a.unshift([3]);
-    expect(cmp.state.a).to.deep.equal([3, 4, 5, 6]);
-    a.unshift([2, 1]);
-    expect(cmp.state.a).to.deep.equal([1, 2, 3, 4, 5, 6]);
-  });
-
-  it('splice', function () {
-    var cmp = renderComponentWithState({a: [1, 2, 3]});
-    var c = Cursor.build(cmp);
-    var a = c.refine('a');
-    a.splice([[1, 1, 4]]);
-    expect(cmp.state.a).to.deep.equal([1, 4, 3]);
-    a.splice([[0, 1, 6, 5], [4, 0, 2, 1]]);
-    expect(cmp.state.a).to.deep.equal([6, 5, 4, 3, 2, 1]);
-  });
-
-  it('merge', function () {
-    var cmp = renderComponentWithState({a: {b: 64}});
-    var c = Cursor.build(cmp);
-    var a = c.refine('a');
-    a.merge({ c: 72 });
-    expect(cmp.state.a).to.deep.equal({ b: 64, c: 72});
-  });
-
-  it('apply against react state', function () {
-    var cmp = renderComponentWithState({a: 64 });
-    var c = Cursor.build(cmp);
-    var a = c.refine('a');
-    a.apply((root) => (leaf) => leaf/8);
-    expect(cmp.state.a).to.equal(8);
-  });
-
-  it('apply against store', function () {
-    cur.refine('a').apply((root) => (leaf) => leaf/8);
-    expect(store.value().a).to.equal(8);
-  });
 });
