7c30896f3508b73f1e21bd0f3c1857ae9f0f0a34
Dustin Getz <dustin.getz@gmail.com>
2016-05-18 09:00:28 -0400

    Add babel cli, add lib target, build all targets and bump version

--- webpack.config.js
+++ webpack.config.js
@@ -30,5 +30,21 @@ module.exports = {
       {test: /\.js$/, loaders: ['babel'], include: path.resolve('./src')},
       {test: /node_modules\/update-in/, loader: 'babel'}
     ]
+  },
+
+  externals: {
+    'react': {
+      'commonjs': 'react',
+      'commonjs2': 'react',
+      'amd': 'react',
+      // React dep should be available as window.React, not window.react
+      'root': 'React'
+    },
+    'react-dom': {
+      'commonjs': 'react-dom',
+      'commonjs2': 'react-dom',
+      'amd': 'react-dom',
+      'root': 'ReactDOM'
+    }
   }
 };
